6108aa2e72b0466aa9a13d524c7cd29e3565fc49
Rajan Khullar <rkhullar@nyit.edu>
2016-06-08 22:59:49 -0400

    clean up

--- app/src/main/java/edu/nyit/app/demo/activity/Alpha.java
+++ app/src/main/java/edu/nyit/app/demo/activity/Alpha.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.Context;
-import android.content.pm.ActivityInfo;
 import android.os.Vibrator;
 import android.widget.EditText;
 
